85134ce51acd41f3edff276a960b0e3c77fd9056
Michael Giamellaro <giamellm@oregonstate.edu>
2026-04-02 17:53:32 -0700

    Update sw.js to v40

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js (PRODUCTION: maximum offline reliability)
-const CACHE_NAME = 'tcm-pwa-v39'; // ✅ bump this on every release
+const CACHE_NAME = 'tcm-pwa-v40'; // ✅ bump this on every release
 
 const ASSETS = [
   './',
