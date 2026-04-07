3167fa16193c093495c2866bc28d6fa072b29f1a
Michael Giamellaro <giamellm@oregonstate.edu>
2026-04-07 08:41:03 -0700

    Update sw.js to v63

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js (PRODUCTION: maximum offline reliability)
-const CACHE_NAME = 'tcm-pwa-v62'; // ✅ bump this on every release
+const CACHE_NAME = 'tcm-pwa-v63'; // ✅ bump this on every release
 
 const ASSETS = [
   './',
